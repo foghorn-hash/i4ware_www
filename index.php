094da232ee7dd1a558f7e6adfe90f7c62ff390ef
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2025-03-30 11:03:02 +0300

    First Commit.

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>i4ware Software – Räätälöityä ohjelmistokehitystä</title>
+    <meta name="description" content="i4ware Software tarjoaa räätälöityjä ohjelmistoratkaisuja. Tutustu palveluihimme ja ota yhteyttä tänään.">
+    <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
+    <link rel="stylesheet" href="static/css/main.fb961db8.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <img src="assets/i4ware-software.png" alt="i4ware Software Logo" class="logo" />
+            <div class="header-text">
+                <h1>Mitä teemme?</h1>
+                <p>Luomme koodia, joka ratkaisee ongelmasi.</p>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="services">
+            <h2>Palvelumme</h2>
+            <div class="image-container">
+                <img src="assets/dreamstime_s_158835340.jpg" alt="Koodia" />
+                <img src="assets/dreamstime_m_50188994.jpg" alt="Koodia" />
+            </div>
+            <p>Tarjoamme kattavia ohjelmointipalveluita, jotka räätälöidään tarpeidesi mukaan. Osaamisemme kattaa:</p>
+            <ul>
+                <li>Web-kehityksen moderneilla teknologioilla (React, Laravel, jne.)</li>
+                <li>Integraatiot ja API-kehitys</li>
+                <li>Verkkosivustojen ja sovellusten suunnittelu ja toteutus</li>
+                <li>Ohjelmistokehitys ja -ylläpito</li>
+                <li>Tehokkaat pilviratkaisut</li>
+            </ul>
+            <h2>Teknologiat:</h2>
+            <p>Tomcat, Apache 2, Python, Java EE, Spring Boot, Ubuntu Server, Red Hat Enterprise Linux, MySQL, PostgreSQL, PHP, Laravel, React, TypeScript, CSS, HTML, XML, AJAX, REST, RESTful, JSON, JSONP, JavaScript, Node.js, Firebase (NoSQL-tietokanta), Firebase (PHP-kehys JSON Web Tokenille), React Native, C#</p>
+            <p>Ota meihin yhteyttä, niin keskustellaan tarkemmin tarpeistasi ja siitä, miten voimme auttaa sinua saavuttamaan tavoitteesi.</p>
+            <p><img src="assets/marketplace_partner_wht_nobg.png" class="partner-logo" alt="Atlassian Marketplace Partner Logo" /></p>
+        </section>
+
+        <section class="testimonials">
+            <div class="image-container">
+                <img src="assets/dreamstime_xl_31414185.jpg" alt="Matematiikkaa" />
+            </div>  
+            <h2>Asiakaspalautteet</h2>
+            <p>"i4ware Software ratkaisi liiketoimintamme kriittiset IT-haasteet nopeasti ja ammattitaitoisesti."</p>
+            <p>– Tyytyväinen asiakas</p>
+            <div class="testimonial">
+            <p><strong>Vili Vepsäläinen</strong>, Kehitysjohtaja (Doberman Consulting Oy) ja Koulutussuunnittelija (Edupoli):  
+            ”Mielestäni Matti Kiviharju suoritti tehtävänsä kiitettävällä nopeudella ja tehokkuudella. Hän toi työhön oman luovan panoksensa ja onnistui kehittämään työryhmänsä toimintaa työn loppuun saattamiseksi. Matti Kiviharju toteutti Etelä-Uudenmaan koulutuskuntayhtymän (Edupoli) ADP-tiimin verkkosivuston kehitysprojektin etätyönä harjoittelijana. Hän hoiti tehtävänsä harjoittelijatiimin projektipäällikkönä erittäin hyvin.”</p>
+            <a href="https://www.linkedin.com/in/vilivepsalainen/" target="_blank">LinkedIn-profiili</a>
+        </div>
+        
+        <div class="testimonial">
+            <p><strong>Olli Saranen</strong>, Toimitusjohtaja, Avoset Oy:  
+            ”Matti Kiviharjun tehtävät tulevat suoritetuiksi luovalla tavalla, ja hän tekee aina sen, mitä sopimuksessa on sovittu. Matti sopii ohjelmistokehitysalalle ohjelmoijana, ja se on hänen tulevaisuuden tehtävänsä.”</p>
+            <a href="https://www.linkedin.com/in/ollisaranen/" target="_blank">LinkedIn-profiili</a>
+        </div>
+        
+        <div class="testimonial">
+            <p><strong>Henri Memonen</strong>, Toimitusjohtaja:  
+            ”Matti on erittäin luova ja tekee aina tilatut työt. Olen työskennellyt hänen kanssaan vuodesta 2013. Hän on toteuttanut erittäin laajoja järjestelmiä. Suosittelen todella lämpimästi Matti Kiviharjua kaikille ja tarkoitan sitä.”</p>
+            <a href="https://www.linkedin.com/in/henri-memonen/" target="_blank">LinkedIn-profiili</a>
+        </div>
+        
+        <div class="testimonial">
+            <p><strong>Mikko Mäkelä</strong>, Toimitusjohtaja:  
+            ”Matti on äärimmäisen kykenevä hoitamaan useita tehtäviä samanaikaisesti. Hän oli tiimimme johtaja suuressa verkkosivustoportaaliprojektissa täällä Suomessa. Hänellä on vahvat johtamistaidot ja erinomainen sinnikkyys, jotka auttavat hallitsemaan stressiä.”</p>
+            <a href="https://www.linkedin.com/in/ruffnekk/" target="_blank">LinkedIn-profiili</a>
+        </div>
+
+        <div class="testimonial">
+            <p><strong>Pauli Kiviharju</strong>, Asianajaja, Asianajotoimisto Pauli Kiviharju Ky:  
+            ”Matti Kiviharju toteutti verkkosivut toimistolleni, tarjoten erityisesti IT/ICT-alan asianajajan tarpeisiin räätälöityjä palveluita. Hän työskenteli ammattitaitoisesti ja toimitti ratkaisun nopeasti ja tehokkaasti.”</p>
+            <a href="https://www.paulikiviharju.fi/" target="_blank">Kotisivut</a>
+        </div>
+
+        <div class="testimonial">
+            <h2>i4ware Software ja Polarion ALM:n Diagramming Tool</h2>
+            <p>i4ware Software toteutti alun perin <strong>Diagramming Toolin</strong> Polarion ALM -ohjelmistoon vuonna <strong>2010</strong>, jolloin ohjelmiston omisti saksalainen <strong>Polarion Software</strong>. Myöhemmin <strong>Siemens hankki Polarionin</strong>, ja nykyään ohjelmisto tunnetaan nimellä <strong>Siemens Polarion AG</strong>.</p>
+            <p>Tämä työkalu on ollut keskeinen osa Polarion ALM:n sovelluksen elinkaaren hallinnan ekosysteemiä. i4ware Softwaren ohjelmistoinsinöörien asiantuntemus on parantanut merkittävästi työkalun toiminnallisuutta ja käytettävyyttä.</p>
+            <blockquote>
+                <p><em>"Polarion ALM:n Diagramming Tool, jonka i4ware Software alun perin toteutti vuonna 2010, on ollut keskeinen osa sovelluksen elinkaaren hallinnan ekosysteemiämme. i4ware Softwaren ohjelmistoinsinöörien asiantuntemus on merkittävästi parantanut työkalun toiminnallisuutta ja käytettävyyttä."</em></p>
+                <p>- Siemens Polarion AG</p>
+            </blockquote>
+            <h2>Esittelyvideot</h2>
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/DD8ctrwCfrQ" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/ocOENQhqEjk" allowfullscreen></iframe>
+            </div>
+        </div>
+        </section>
+
+        <section id="team">
+            <h2>Henkilökunta</h2>
+            <div class="team-member">
+                <img src="assets/matti.jpg" alt="Matti Kiviharju" />
+                <h3>Matti Kiviharju, tradenomi</h3>
+                <p>Yrittäjä, Perustaja ja Expert Full-Stack -ohjelmoija sekä arkkitehti</p>
+                <p>Matti Kiviharju on kokenut ohjelmistoarkkitehti ja Full-Stack-kehittäjä, joka hallitsee sekä käyttöliittymien että backend-järjestelmien suunnittelun. Hänen visionäärinen johtajuutensa ja tekninen asiantuntemuksensa ovat olleet avainasemassa yrityksen menestyksessä.</p>
+            </div>
+        </section>
+
+        <section class="contact" id="contact">
+            <h2>Ota yhteyttä</h2>
+            <p>Olemme täällä auttamassa sinua. Ota meihin yhteyttä sähköpostitse: <a href="mailto:info@i4ware.fi">info@i4ware.fi</a></p>
+            <p>ALV-rek. FI27395946</p>
+
+            <h1>Perustelut vajaakuntoisen koodarin tilaamiselle</h1>
+
+            <h2>1. Verotukselliset edut</h2>
+            <p>Vajaakuntoisena työntekijänä minulla on oikeus invaliidivähennykseen, mikä tarkoittaa, että eläkkeestäni ei mene lainkaan ennakonpidätystä. Tämä verohyöty tekee nettotuloistani merkittävästi korkeammat, vaikka bruttotulot olisivat verrattain alhaisemmat. Lisäksi työkorvauksista verotetaan vain osuus, joka ylittää kiinteän 980,30 €/kk + ALV 25,5 %, mikä pitää kokonaisveroasteen matalana.</p>
+
+            <h2>2. Neljä tulonlähdettä</h2>
+            <p>Eläke- ja tukijärjestelmäni mahdollistaa neljä eri tulonlähdettä kuukaudessa:</p>
+            <ul>
+                <li><strong>Eläke:</strong> 783,41 €/kk</li>
+                <li><strong>Asumistuki:</strong> 470,09 €/kk</li>
+                <li><strong>Takuueläke:</strong> 202,89 €/kk</li>
+                <li><strong>Työkorvaus:</strong> Jopa 980,30 €/kk + ALV 25,5 %</li>
+            </ul>
+            <p>Yhteensä tämä mahdollistaa kuukausitulot, jotka ovat korkeammat kuin monilla täysipäiväisillä työntekijöillä, kuten sairaanhoitajilla, joiden bruttopalkka on noin 2500 €/kk ilman lisiä.</p>
+
+            <h2>3. Kustannustehokkuus työn tilaajalle</h2>
+            <p>Työn tilaajalle palveluni ovat kustannustehokkaita, koska:</p>
+            <ul>
+                <li><strong>Kiinteä kuukausihinta:</strong> Työni hinnoitellaan selkeästi 980,30 €/kk + ALV 25,5 %.</li>
+                <li><strong>Ei ylitöiden lisäkuluja:</strong> Olen valmis tekemään työtä joustavasti ilman lisäkustannuksia.</li>
+                <li><strong>YEL-vakuutusmaksun optimointi:</strong> YEL-työtuloni on asetettu tasolle, joka maksimoi kustannustehokkuuden työn tilaajan näkökulmasta.</li>
+            </ul>
+
+            <h2>4. Korkea nettotulo</h2>
+            <p>Vajaakuntoisena koodarina nettotuloni ovat merkittävästi korkeammat suhteessa bruttotuloihin verrattuna täysipäiväiseen työntekijään. Tämä tarkoittaa, että saan enemmän käteen samalla hinnalla, jolla työn tilaaja saa laadukasta ja kilpailukykyistä palvelua.</p>
+
+            <h2>5. Eettinen ja sosiaalinen vastuu</h2>
+            <p>Työllistämällä vajaakuntoisen työntekijän työn tilaaja osoittaa yhteiskunnallista vastuullisuutta ja edistää inklusiivista työkulttuuria. Tämä voi parantaa yrityksen mainetta ja antaa positiivista näkyvyyttä niin asiakkaiden kuin yhteistyökumppaneidenkin silmissä.</p>
+
+            <p>Ota yhteyttä, niin keskustellaan tarkemmin yhteistyömahdollisuuksista!</p>
+            <div id="root"></div>
+            <script src="static/js/main.ba5695a1.js"></script>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2025 i4ware Software. Kaikki oikeudet pidätetään.</p>
+    </footer>
+</body>
+</html>
