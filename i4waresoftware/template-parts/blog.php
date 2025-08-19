13dfdc27c99bdf4b984d9121848ea0c4b2724d6f
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2025-08-19 18:04:13 +0300

    Commit.

--- i4waresoftware/template-parts/blog.php
+++ i4waresoftware/template-parts/blog.php
@@ -67,3 +67,12 @@ $lang = function_exists('pll_current_language') ? pll_current_language() : 'fi';
         <?php endif; ?>
     </div>
 </div>
+<div class="footer-social">
+    <!-- Social Media Links -->
+    <a href="https://www.youtube.com/@i4wareSoftware-ot5jk" target="_blank" rel="noopener" title="YouTube" style="margin-right:10px;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+            <path d="M8.051 1.999h-.002C3.634 2.017 2.01 2.2 1.292 2.482c-.72.282-1.28.87-1.292 1.684C0 5.36 0 8 0 8s0 2.64.001 3.834c.012.814.572 1.402 1.292 1.684.718.282 2.342.465 6.757.483h.002c4.415-.018 6.039-.201 6.757-.483.72-.282 1.28-.87 1.292-1.684C16 10.64 16 8 16 8s0-2.64-.001-3.834c-.012-.814-.572-1.402-1.292-1.684-.718-.282-2.342-.465-6.757-.483zM6.545 10.568V5.432l4.545 2.568-4.545 2.568z"/>
+        </svg>
+    </a>
+    <!-- Add more social links here if needed -->
+</div>
