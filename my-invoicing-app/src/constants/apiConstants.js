65f78235c9fab1d9ec31310a4a8eedd756d2b965
Matti Kiviharju <matti.kiviharju@i4ware.fi>
2026-02-21 18:18:54 +0200

    Initial Commit.

--- my-invoicing-app/src/constants/apiConstants.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
-export const API_DEFAULT_LANGUAGE = process.env.REACT_APP_DEFAULT_LANGUAGE;
--- saas-app/resources/views/response.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        @highlight('php') {{-- Assuming the response contains PHP code --}}
-        {!! $response !!}
-        @endhighlight
-    </div>
-@endsection
